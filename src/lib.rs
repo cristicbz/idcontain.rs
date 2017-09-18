e2ca0dde10accb4aef9fca2bb543effc6b774144
Cristi Cobzarenco <cristi.cobzarenco@gmail.com>
2017-09-18 20:55:59 +0100

    remove skeptic for now :(

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ mod slab;
 
 pub use flat::{FlatAccess, FlatAccessMut, Flat, FlatGet, FlatGetMut};
 pub use id::{IdTag, Id, MAXIMUM_CAPACITY};
-pub use map::IdMap;
+pub use map::{IdMap, IdMapVec};
 pub use vec::IdVec;
 pub use option::OptionId;
 pub use slab::{IdSlab, Iter, IterMut};
